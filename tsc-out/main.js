e7d8de2fcf73e999db787ed2a6ba380768685e65
0x13f <mr0x13f@gmail.com>
2020-05-26 18:23:31 +0200

    removed tsc-out

--- tsc-out/main.js
+++
@@ -1,7 +0,0 @@
-import { World } from "./World";
-import { Car } from "./Car";
-window.onload = function () {
-    // Start de simulatie
-    World.init();
-    var car = new Car("js--car"); // Verbind een Car entity aan #js--car
-};
