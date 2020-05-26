7519f9a267cdedb2b7b1029ff63abd089ba6c864
0x13f <mr0x13f@gmail.com>
2020-05-26 18:32:54 +0200

    moved car into src/entities/

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { World } from "./World"
-import { Car } from "./Car";
+import { Car } from "./entities/Car";
 
 window.onload = () => {
 
     // Start de simulatie
     World.init();
 
-    let car:Car = new Car("js--car"); // Verbind een Car entity aan #js--car
+    let car:Car = new Car("car"); // Verbind een Car entity aan #car
 
 }
